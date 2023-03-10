c857b77205ee96c326b88673b625f6939310de66
samsostudios <sam@samsostudios.com>
2023-03-10 13:19:33 -0700

    gsap test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+import { gsap } from 'gsap';
+import { SplitText } from 'gsap/SplitText';
+
+gsap.registerPlugin(SplitText);
+
 window.Webflow ||= [];
 window.Webflow.push(() => {
   console.log('pnpm gsap business');
+
+  const element = document.querySelector('#splitThis');
+  const splitMenu = new SplitText(element, { type: 'words,chars' });
+
+  const tl = gsap.timeline();
+  tl.to(splitMenu.chars, { y: '1rem', stagger: { each: 0.2 } });
+  console.log('SPLIT', splitMenu);
 });
