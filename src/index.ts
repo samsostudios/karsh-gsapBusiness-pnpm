4d1b9f6caa276f19c6553ecad86239eabb4be9a4
samsostudios <sam@samsostudios.com>
2023-03-10 13:23:53 -0700

    bg change

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,11 @@ window.Webflow.push(() => {
   console.log('pnpm gsap business');
 
   const element = document.querySelector('#splitThis');
+  const body = document.querySelector('body');
   const splitMenu = new SplitText(element, { type: 'words,chars' });
 
   const tl = gsap.timeline();
   tl.to(splitMenu.chars, { y: '1rem', stagger: { each: 0.2 } });
+  tl.to(body, { backgroundColor: 'yellow' }, '<');
   console.log('SPLIT', splitMenu);
 });
